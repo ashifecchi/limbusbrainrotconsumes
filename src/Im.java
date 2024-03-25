a732a42e49f252e5693b5c5ed7a787159f616c74
BT_1N3_30 <@>
2024-03-25 11:03:20 -0400

    did some stuff for a pool

--- src/Im.java
+++ src/Im.java
@@ -7,24 +7,32 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.sql.Array;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 
 import static java.lang.Thread.currentThread;
 import static java.lang.Thread.sleep;
 
 public class Im extends JPanel implements ActionListener {
+    private ArrayList<Chars> pool;
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         //buttons
-        JButton gacha = new JButton("gacja");
+        JButton gacha = new JButton();
         gacha.setSize(125,30);
-        gacha.setVisible(false);
         gacha.setEnabled(true);
+        gacha.setOpaque(false); //https://stackoverflow.com/questions/5654208/making-a-jbutton-invisible-but-clickable thank you random guy
+        gacha.setContentAreaFilled(false);
+        gacha.setBorderPainted(false);
         gacha.setLocation(800,470);
         gacha.addActionListener(this);
 
@@ -36,9 +44,22 @@ public class Im extends JPanel implements ActionListener {
             throw new RuntimeException(e);
         }
 
+        //POOl !!!
+        ArrayList<Chars> pool = new ArrayList<>();
+        pull puller = new pull(pool);
+
         //draw
         g.drawImage( bg,0,0,this);
         this.add(gacha);
+    }
+    private void initializeCharPool() throws IOException {
+        pool = new ArrayList<>();
+        Scanner filescanner = new Scanner(new File("src/CharacterNames"));
+        Scanner file2scanner = new Scanner(new File("src/CharacterImgs"));
+        while (filescanner.hasNext()){
+            Chars c = new Chars(filescanner.nextLine(),loadImg(file2scanner.nextLine()));
+            pool.add(c);
+        }
     }
     public static BufferedImage loadImg(String path) throws IOException {
         return ImageIO.read(new File(path));
